df79ef792baa44cb9c766e7793307fbe7803b1ae
sangmin song <songsari0313@gmail.com>
2022-08-11 13:34:01 +0900

    d_tree function

--- Generate-decision-tree-manually-main/Generate-decision-tree-manually-main/input.py
+++ Generate-decision-tree-manually-main/Generate-decision-tree-manually-main/input.py
@@ -1,9 +1,9 @@
+from re import X
 import pandas as pd
 import manual_tree as mt
 from sklearn.datasets import load_breast_cancer
 
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 import warnings
 warnings.filterwarnings('ignore')
@@ -31,11 +31,6 @@ df1 = pd.DataFrame(cancer['data'], columns=['v'+str(i) for i in range(30)])
 df2 = pd.DataFrame(cancer['target'], columns=['Y'])
 train = pd.concat([df1, df2], axis=1)
 valid = pd.concat([df1, df2], axis=1)
-
-print(df1.columns)
-print(df1.shape)
-print(df2.shape)
-
 #===========================================
 
 
@@ -59,14 +54,13 @@ manaul_obj = mt.Manaul(train, valid, y_label, save_path, model_type, criterion, 
 #===========================================
 #STEP.2
 # generate DecicionTreeClassifier 
-dt_clf = DecisionTreeClassifier(random_state=156)
+# dt_clf = DecisionTreeClassifier(random_state=156)
 
-# 붓꽃 데이터를 로딩하고, 학습과 테스트 데이터 세트로 분리
-iris_data = load_iris()
-X_train, X_test, y_train, y_test = train_test_split(iris_data.data, iris_data.target, test_size=0.2, random_state=11)
+# load dataset from step0, seperate train and test set
 
+# X_train, X_test, y_train, y_test = train_test_split(df1, df2, test_size=0.2, random_state=11)
 # DecisionTreeClassifier 학습
-dt_clf.fit(X_train, y_train)
+# dt_clf.fit(X_train, y_train)
 #===========================================
 
 
@@ -74,24 +68,42 @@ dt_clf.fit(X_train, y_train)
 #===========================================
 #STEP.3
 #prototype modeling
-def prototype():
+def prototype(row):
 
     while True:
+        #generate DecicionTreeClassifier 
+        dt_clf = DecisionTreeClassifier(random_state=156)    
 
-        input_ =(None)    
-
+        var_list = []    
         for i in range(3):
 
             manaul_obj.get_pool_node_id(i)  # divide the data into two pools i and i+1 according to the feature V(text)
             text = input('variable ex)v13 :  ')
+
+
             value = float(input('split_value  ex)14 :  '))
             manaul_obj.calculate_feature_split(variable_selected = text, split_value = value) #v20
             manaul_obj.save_step_split()
             print('\n')
+            var_list.append(text)
 
-        output_ = (None)
+        # load data
+        X_train = df1[var_list]
+        y_train = df2
+        # train model for d_tree
+        dt_clf.fit(X_train, y_train)
+        # return ouput
+        input_x = X_train.iloc[row] #select rows for inferencing
+        input_x = pd.DataFrame(input_x) #reshape for acceptable input
+        input_reshape_x = input_x.transpose() #(1, number of feature)
+           
+        output_y = dt_clf.predict(input_reshape_x)
+        print(output_y)
+
+        return output_y
+        
 #============================================
 
 
 
-prototype()
+prototype(40)
